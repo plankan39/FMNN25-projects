72888a602ff5e6ef9498bca62e47f0b2fa71674f
Swirskey <stan.swirski@gmail.com>
2023-09-25 21:23:02 +0200

    first draft of something

---
+++ project_2/src/somefunction.py
@@ -0,0 +1,23 @@
+import numpy as np
+def newton(f,point,epsilon,max_iter):
+    i = 0
+    next_point = point - np.dot(HESSIAN,GRADIENT)
+    while  np.linalg.norm(point - next_point) >  epsilon and i < max_iter:
+        
+        i += 1
+        next_point = point - np.dot(HESSIAN,GRADIENT)
+
+def calculate_gradient(f, p, epsilon=1e-5):
+   
+    p = np.array(p)
+    
+   
+    gradient = np.zeros_like(p)
+    
+    
+    for i in range(len(p)):
+        
+        gradient[i] = (f(p+epsilon) - f(p)) / epsilon
+        
+    return gradient
+ 
