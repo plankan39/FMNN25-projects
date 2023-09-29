d6efdd25a6a9184e4eda1ee06d627c6cd23c6e19
Lukas Elmlund <lukas.elmlund@gmail.com>
2023-09-29 21:08:36 +0200

    task 7 added, need fixing

---
+++ project_2/src/task_7.py
@@ -0,0 +1,25 @@
+from pprint import pprint
+
+import numpy as np
+from line_search import PowellWolfeScipy
+from optimization import Problem
+from optimization.newton import NewtonWithLineSearch
+from plot_optimization import plot2dOptimization
+
+
+def rosenbrock(x):
+    return 100 * (x[1] - x[0] ** 2) ** 2 + (1 - x[0]) ** 2
+
+
+if __name__ == "__main__":
+    problem = Problem(rosenbrock)
+    powellWolfeLineSearch = PowellWolfeScipy(
+        problem.objective_function, problem.gradient_function
+    )
+    optimizer = NewtonWithLineSearch(problem, powellWolfeLineSearch)
+
+    x0 = np.array([0, -0.7])
+    x_min, f_min = optimizer.optimize(x0, epsilon=1e-6, max_iter=100, bk=10)
+    pprint(x_min)
+
+    plot2dOptimization(problem.objective_function, x_min)
