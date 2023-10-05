f42cf1b7310ed052a76e80c31b4ba1a543da3394
Lukas Elmlund <lukas.elmlund@gmail.com>
2023-10-05 12:43:21 +0200

    update

--- project_3/src/solve_single_room.py
+++ project_3/src/solve_single_room.py
@@ -12,30 +12,32 @@ from pprint import pprint
 x_len = 1.0
 y_len = 1.0
 
-# Boundaries
-top_b = 40
-bot_b = 5
-left_b = 15
-right_b = 15
-
 # Number of discrete points along the x-axis
-x_N_tot = 4
+x_N_tot = 30
 
 # The stepsize for the discrete points
 h = x_len / x_N_tot
+print(f"h={h}")
 
 # Number of discrete points along y-axis
 y_N_tot = int(y_len / h)
 
 # As we know the boundary points the number of unknown points along x and y are
-x_N = x_N_tot - 1
-y_N = y_N_tot - 1
+x_N = x_N_tot - 2
+y_N = y_N_tot - 2
 
 # Total number of unknown points
 N = x_N * y_N
 
 print(f"x_N = {x_N}, y_N = {y_N}, N = {N}")
 
+# Boundaries
+top_b = [40] * x_N_tot
+bot_b = [5] * x_N_tot
+left_b = [15] * y_N_tot
+right_b = [15] * y_N_tot
+
+print(f"left: {left_b}")
 
 ######################### Constructing Laplace matrix #########################
 
@@ -43,43 +45,48 @@ print(f"x_N = {x_N}, y_N = {y_N}, N = {N}")
 d_0 = -4 * np.ones(N)
 
 # The agacent diagonals under and over d_0
-d_1 = np.ones(N-1)
+d_1 = np.ones(N - 1)
 # Change the points where Boundary conditions are imposed
-d_1[[k*y_N - 1 for k in range(1, x_N)]] = 0
+d_1[[k * y_N - 1 for k in range(1, x_N)]] = 0
 
 # The two last diagonals that are y_N
-d_y_N = np.ones(N-y_N)
+d_y_N = np.ones(N - y_N)
 
 # The Laplace matrix
 A = sp.sparse.diags([d_0, d_1, d_1, d_y_N, d_y_N], [0, 1, -1, y_N, -y_N], format="csc")
-
+print(f"A:\n{A.toarray()}")
 # The known boundary conditions
 b = np.zeros(N)
-b[-y_N:] -= right_b
-b[:y_N-1] -= left_b
-b[::y_N] -= bot_b
-b[y_N-1::y_N] -= top_b
 
-theta = sp.sparse.linalg.spsolve(A,b)
+b[-y_N:] -= right_b[1:-1]
+b[: y_N] -= left_b[1:-1]
+b[::y_N] -= bot_b[1:-1]
+b[y_N - 1 :: y_N] -= top_b[1:-1]
+print(f"b:\n{b}")
+theta = sp.sparse.linalg.spsolve(A, b)
 
-X, Y = np.meshgrid(np.linspace(0, x_len, x_N_tot + 1), np.linspace(0, y_len, y_N_tot + 1))
+# print(theta)
+
+X, Y = np.meshgrid(np.linspace(0, x_len, x_N_tot), np.linspace(0, y_len, y_N_tot))
 
 T = np.zeros_like(X)
 
-T[-1,:] = top_b
-T[0,:] = bot_b
-T[:,0] = left_b
-T[:,-1] = right_b
 
-for j in range(1, y_N_tot):
-    for i in range(1, x_N_tot):
-        T[j, i] = theta[j + (i-1)*y_N -1]
+T[-1, :] = top_b
+T[0, :] = bot_b
+T[:, 0] = left_b
+T[:, -1] = right_b
 
+for j in range(1, y_N_tot - 1):
+    for i in range(1, x_N_tot - 1):
+        T[j, i] = theta[j + (i - 1) * y_N - 1]
 
-print(T)
+print(f"T:\n{T}")
+
+# print(T)
 
 plt.title("Temperature in the room")
-plt.contourf(X, Y, T, levels = 50, cmap = plt.cm.coolwarm)
+plt.contourf(X, Y, T, levels=1000, cmap=plt.cm.coolwarm)
 
 plt.colorbar()
 
