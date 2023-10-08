afda67e31bd70314481483ba0bbbe043d6b5b73e
Lukas Elmlund <lukas.elmlund@gmail.com>
2023-10-08 12:35:52 +0200

    functionize

--- project_3/src/solve_single_room.py
+++ project_3/src/solve_single_room.py
@@ -1,7 +1,11 @@
 import numpy as np
-import scipy as sp
+from scipy.sparse import diags, linalg
 import matplotlib.pyplot as plt
 from pprint import pprint
+
+
+def construct_neumann(unknown_boundry):
+    pass
 
 
 def construct_matrix(x_N: int, y_N: int):
@@ -10,11 +14,14 @@ def construct_matrix(x_N: int, y_N: int):
     # The agacent diagonals under and over d_0
     d_1 = np.ones(x_N * y_N - 1)
     # Change the corner points where Boundary conditions are imposed.
+    for k in range(1, x_N):
+        d_1[k * y_N - 1] = 0
+
     d_1[[k * y_N - 1 for k in range(1, x_N)]] = 0
     # The two last diagonals that are y_N
     d_y_N = 1
 
-    return sp.sparse.diags(
+    return diags(
         [d_0, d_1, d_1, d_y_N, d_y_N],
         [0, 1, -1, y_N, -y_N],
         shape=(x_N * y_N, x_N * y_N),
@@ -37,7 +44,7 @@ def generate_boundry(
     return b
 
 
-def reconstruct_result_matrix_with_boundries(theta: np.ndarray, x_N, y_N):
+def reconstruct_result_matrix_with_boundries(theta: np.ndarray, x_N: int, y_N):
     return theta.reshape(y_N, x_N, order="F")
 
 
@@ -47,33 +54,28 @@ if __name__ == "__main__":
     # Describe the length of the sides of the rectangle
     x_len = 1.0
     y_len = 1.0
-
     # Number of discrete points along the x-axis
-    x_N_tot = 3
+    x_N = 2
 
     # The stepsize for the discrete points
-    h = x_len / x_N_tot
+    h = x_len / x_N
 
     # Number of discrete points along y-axis
-    y_N_tot = int(y_len / h)
-
-    # As we know the boundary points the number of unknown points along x and y are
-    x_N = x_N_tot  # - 2
-    y_N = y_N_tot  # - 2
+    y_N = int(y_len / h)
 
     # Total number of unknown points
     N = x_N * y_N
 
     # Boundaries
-    top_b = 40 * np.ones(x_N_tot)
-    bot_b = 5 * np.ones(x_N_tot)
-    left_b = 15 * np.ones(y_N_tot)
-    right_b = 15 * np.ones(y_N_tot)
+    top_b = 40 * np.ones(x_N)
+    bot_b = 5 * np.ones(x_N)
+    left_b = 15 * np.ones(y_N)
+    right_b = 15 * np.ones(y_N)
 
     A = construct_matrix(x_N, y_N)
 
     b = generate_boundry(left_b, right_b, bot_b, top_b)
-    theta = sp.sparse.linalg.spsolve(A, b)
+    theta = linalg.spsolve(A, b)
 
     T = reconstruct_result_matrix_with_boundries(theta, x_N, y_N)
 
@@ -84,7 +86,7 @@ if __name__ == "__main__":
     print(f"b:\n{b}")
     print(f"T:\n{T}")
 
-    X, Y = np.meshgrid(np.linspace(0, x_len, x_N_tot), np.linspace(0, y_len, y_N_tot))
+    X, Y = np.meshgrid(np.linspace(0, x_len, x_N), np.linspace(0, y_len, y_N))
     plt.title("Temperature in the room")
     plt.contourf(X, Y, T, levels=500, cmap=plt.cm.coolwarm)
     plt.colorbar()
