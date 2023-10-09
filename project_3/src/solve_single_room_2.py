731c7276971dfd45a8ad5d80ad9359bcc14e1117
Lukas Elmlund <lukas.elmlund@gmail.com>
2023-10-09 16:07:27 +0200

    formatting

--- project_3/src/solve_single_room_2.py
+++ project_3/src/solve_single_room_2.py
@@ -17,9 +17,6 @@ class Boundary:
     bot_dirichlet: np.ndarray
 
 
-<< << << < HEAD
-
-
 def calculate_neuman_values(boundary: Boundary, temperature: np.ndarray):
     left = np.zeros_like(boundary.left_dirichlet)
     for i, b in enumerate(boundary.left_dirichlet):
@@ -73,9 +70,9 @@ def solve_dirichlet(x_N: int, y_N: int, boundary: Boundary):
     d2 = np.zeros(N - y_N)
 
     for i in range(1, x_N - 1):
-        d0[i * y_N + 1: (i + 1) * y_N - 1] = -4
-        d1[i * y_N + 1: (i + 1) * y_N - 1] = 1
-        d2[i * y_N + 1: (i + 1) * y_N - 1] = 1
+        d0[i * y_N + 1 : (i + 1) * y_N - 1] = -4
+        d1[i * y_N + 1 : (i + 1) * y_N - 1] = 1
+        d2[i * y_N + 1 : (i + 1) * y_N - 1] = 1
 
     # create matrix with dirichlet conditions
     A = diags(
@@ -90,7 +87,7 @@ def solve_dirichlet(x_N: int, y_N: int, boundary: Boundary):
     b[:y_N] = boundary.left
     b[-y_N:] = boundary.right
     b[::y_N] = boundary.bot
-    b[y_N - 1:: y_N] = boundary.top
+    b[y_N - 1 :: y_N] = boundary.top
 
     print(b)
 
@@ -106,9 +103,9 @@ def solve_neumann(x_N: int, y_N: int, boundary: Boundary):
     d2 = np.zeros(N - y_N)
 
     for i in range(1, x_N - 1):
-        d0[i * y_N + 1: (i + 1) * y_N - 1] = -4
-        d1[i * y_N + 1: (i + 1) * y_N - 1] = 1
-        d2[i * y_N + 1: (i + 1) * y_N - 1] = 1
+        d0[i * y_N + 1 : (i + 1) * y_N - 1] = -4
+        d1[i * y_N + 1 : (i + 1) * y_N - 1] = 1
+        d2[i * y_N + 1 : (i + 1) * y_N - 1] = 1
 
     A = diags(
         [np.flip(d2), np.flip(d1), d0, d1, d2], [-y_N, -1, 0, 1, y_N], format="csc"
@@ -118,7 +115,7 @@ def solve_neumann(x_N: int, y_N: int, boundary: Boundary):
     b[:y_N] = boundary.left
     b[-y_N:] = boundary.right
     b[::y_N] = boundary.bot
-    b[y_N - 1:: y_N] = boundary.top
+    b[y_N - 1 :: y_N] = boundary.top
 
     print(b)
 
@@ -134,100 +131,56 @@ if __name__ == "__main__":
     x_N_13 = 4
     y_N_13 = x_N_13
 
-# masks of shared boundraies
-gamma_1_mask = np.concatenate((1, np.zeros(y_N_13 - 2), 1))
-gamma_2_mask = gamma_1_mask
+    # masks of shared boundraies
+    gamma_1_mask = np.concatenate((1, np.zeros(y_N_13 - 2), 1))
+    gamma_2_mask = gamma_1_mask
 
-omega_1 = Boundary(
-    heater * np.ones(y_N_13),  # left
-    wall * np.ones(x_N_13),  # top
-    wall * np.ones(y_N_13),  # right, shared with omega 2
-    wall * np.ones(x_N_13),  # bottom
-    np.ones(y_N_13),  # left,
-    np.ones(x_N_13),  # top,
-    gamma_1_mask,  # right, this is shared have some zeros
-    np.ones(x_N_13),  # bottom
-)
-temp, A, b = solve_dirichlet(x_N_13, y_N_13, omega_1)
-print(temp)
-print(calculate_neuman_values(omega_1, temp))
+    omega_1 = Boundary(
+        heater * np.ones(y_N_13),  # left
+        wall * np.ones(x_N_13),  # top
+        wall * np.ones(y_N_13),  # right, shared with omega 2
+        wall * np.ones(x_N_13),  # bottom
+        np.ones(y_N_13),  # left,
+        np.ones(x_N_13),  # top,
+        gamma_1_mask,  # right, this is shared have some zeros
+        np.ones(x_N_13),  # bottom
+    )
+    temp, A, b = solve_dirichlet(x_N_13, y_N_13, omega_1)
+    print(temp)
+    print(calculate_neuman_values(omega_1, temp))
 
-# Omega 2
-x_N_2 = x_N_13
-y_N_2 = 2 * x_N_2
+    # Omega 2
+    x_N_2 = x_N_13
+    y_N_2 = 2 * x_N_2
 
-omega_2 = Boundary(
-    wall * np.ones(y_N_2),  # left, partially shared with omega_1
-    heater * np.ones(x_N_2),  # top
-    wall * np.ones(y_N_2),  # right, partially shared with omega_3
-    window * np.ones(x_N_2),  # bot
-    np.concatenate((gamma_1_mask, np.ones(
-        y_N_2 - len(gamma_1_mask)))),  # left
-    np.ones(x_N_2),  # top
-    np.concatenate(
-        (np.ones(y_N_2 - len(gamma_2_mask)), gamma_2_mask)),  # right
-    np.ones(x_N_2),  # bot
-)
+    omega_2 = Boundary(
+        wall * np.ones(y_N_2),  # left, partially shared with omega_1
+        heater * np.ones(x_N_2),  # top
+        wall * np.ones(y_N_2),  # right, partially shared with omega_3
+        window * np.ones(x_N_2),  # bot
+        np.concatenate((gamma_1_mask, np.ones(y_N_2 - len(gamma_1_mask)))),  # left
+        np.ones(x_N_2),  # top
+        np.concatenate((np.ones(y_N_2 - len(gamma_2_mask)), gamma_2_mask)),  # right
+        np.ones(x_N_2),  # bot
+    )
 
-# Omega 3
-omega_3 = Boundary(
-    heater * np.ones(y_N_13),
-    wall * np.ones(x_N_13),
-    wall * np.ones(y_N_13),
-    wall * np.ones(x_N_13),
-    gamma_2_mask,
-    np.ones(x_N_13),
-    np.ones(y_N_13),
-    np.ones(x_N_13),
-)
+    # Omega 3
+    omega_3 = Boundary(
+        heater * np.ones(y_N_13),
+        wall * np.ones(x_N_13),
+        wall * np.ones(y_N_13),
+        wall * np.ones(x_N_13),
+        gamma_2_mask,
+        np.ones(x_N_13),
+        np.ones(y_N_13),
+        np.ones(x_N_13),
+    )
 
-X, Y = np.meshgrid(np.linspace(0, 1, x_N-2),
-                   np.linspace(0, 1.0 * (y_N-2)/(x_N-2), y_N-2))
-plt.title("Temperature in the room")
-plt.contourf(X, Y, temp[1:-1, 1:-1], levels=500, cmap=plt.cm.coolwarm)
-plt.colorbar()
-plt.show()
+    # X, Y = np.meshgrid(
+    #     np.linspace(0, 1, x_N - 2), np.linspace(0, 1.0 * (y_N - 2) / (x_N - 2), y_N - 2)
+    # )
+    # plt.title("Temperature in the room")
+    # plt.contourf(X, Y, temp[1:-1, 1:-1], levels=500, cmap=plt.cm.coolwarm)
+    # plt.colorbar()
+    # plt.show()
 
-
-# def solve_dirichlet(x_N: int, y_N: int, boundary: Boundary):
-#     A = construct_matrix(x_N, y_N)
-#     b = construct_boundary(boundary)
-#     return linalg.spsolve(A, b)
-
-
-# def construct_matrix(x_N: int, y_N: int):
-#     # Main diagonal
-#     d_0 = -4
-#     # The agacent diagonals under and over d_0
-#     d_1 = np.ones(x_N * y_N - 1)
-#     # Change the corner points where Boundary conditions are imposed.
-#     # for k in range(1, x_N):
-#     #     d_1[k * y_N - 1] = 0
-
-#     d_1[[k * y_N - 1 for k in range(1, x_N)]] = 0
-#     # The two last diagonals that are y_N
-#     d_y_N = 1
-
-#     return diags(
-#         [d_0, d_1, d_1, d_y_N, d_y_N],
-#         [0, 1, -1, y_N, -y_N],
-#         shape=(x_N * y_N, x_N * y_N),
-#         format="csc",
-#     )
-
-
-# def construct_boundary(boundary: Boundary):
-#     x_N = len(boundary.top)
-#     y_N = len(boundary.left)
-
-#     b = np.zeros(x_N * y_N)
-#     b[-y_N:] -= boundary.right
-#     b[:y_N] -= boundary.left
-#     b[::y_N] -= boundary.bot
-#     b[y_N - 1 :: y_N] -= boundary.top
-
-#     return b
-
-
-# def reconstruct_result_matrix_with_boundries(theta: np.ndarray, x_N: int, y_N):
-#     return theta.reshape(y_N, x_N, order="F")
